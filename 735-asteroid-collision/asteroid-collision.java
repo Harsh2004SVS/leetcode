98b826ebbce13e8a989ae734bfeca5842169762c
Harsh2004SVS <vtu25175@veltech.edu.in>
2026-02-04 09:26:31 +0530

    Time: 5 ms (66.05%) | Memory: 46.9 MB (69.00%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (int ast : asteroids) {
+            boolean alive = true;
+
+            while (alive && !stack.isEmpty() && stack.peek() > 0 && ast < 0) {
+                int top = stack.peek();
+
+                if (top < -ast) {
+                    stack.pop(); 
+                } else if (top == -ast) {
+                    stack.pop(); 
+                    alive = false;
+                } else {
+                    alive = false; 
+                }
+            }
+
+            if (alive) {
+                stack.push(ast);
+            }
+        }
+
+        int[] result = new int[stack.size()];
+        for (int i = stack.size() - 1; i >= 0; i--) {
+            result[i] = stack.pop();
+        }
+
+        return result;
+    }
+}
