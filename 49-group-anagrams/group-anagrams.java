d5f09c58c18ccfef0d60a44e495a1b5f561e9635
Harsh2004SVS <vtu25175@veltech.edu.in>
2026-05-03 17:55:12 +0530

    Time: 7 ms (74.18%) | Memory: 49.4 MB (84.33%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -2,21 +2,26 @@ import java.util.*;
 
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-            if (strs == null || strs.length == 0) return new ArrayList<>();
-                    
-                            Map<String, List<String>> map = new HashMap<>();
-                                    
-                                            for (String s : strs) {
-                                                        char[] ca = s.toCharArray();
-                                                                    Arrays.sort(ca);
-                                                                                String key = String.valueOf(ca);
-                                                                                            
-                                                                                                        if (!map.containsKey(key)) {
-                                                                                                                        map.put(key, new ArrayList<>());
-                                                                                                                                    }
-                                                                                                                                                map.get(key).add(s);
-                                                                                                                                                        }
-                                                                                                                                                                
-                                                                                                                                                                        return new ArrayList<>(map.values());
-                                                                                                                                                                            }
-                                                                                                                                                                            } // This should be the very last character in your file
+            // Handle edge case
+                    if (strs == null || strs.length == 0) return new ArrayList<>();
+                            
+                                    Map<String, List<String>> map = new HashMap<>();
+                                            
+                                                    for (String s : strs) {
+                                                                // Sort characters to create a unique key for all anagrams
+                                                                            char[] ca = s.toCharArray();
+                                                                                        Arrays.sort(ca);
+                                                                                                    String key = String.valueOf(ca);
+                                                                                                                
+                                                                                                                            // If key is not present, add a new list
+                                                                                                                                        if (!map.containsKey(key)) {
+                                                                                                                                                        map.put(key, new ArrayList<>());
+                                                                                                                                                                    }
+                                                                                                                                                                                
+                                                                                                                                                                                            // Add original string to the corresponding list
+                                                                                                                                                                                                        map.get(key).add(s);
+                                                                                                                                                                                                                }
+                                                                                                                                                                                                                        
+                                                                                                                                                                                                                                return new ArrayList<>(map.values());
+                                                                                                                                                                                                                                    }
+                                                                                                                                                                                                                                    }
